4b50fb3155974a37db0e89cde08bd08f524c0bf6
bhudevqversity <bhudev.kashyap@qversity.com>
2019-10-10 19:46:38 +0530

    start ui design

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ class Header extends Component {
     render(){
         return(
             <div className="container-fluid">
-                <div className="row top-header">
-                    <div className="col-lg-2">
+                <div className="row top-header p-4">
+                    <div className="col-lg-2 d-flex d-md-block justify-content-center p-2">
                         <img src="/images/login-logo.png" className="logo" />
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 d-flex d-md-block justify-content-center p-4">
                         <div className="user-info d-flex align-items-center">
                             <img className="user-avtar" src="images/attendee.png" />
                             <div className="pl-4">
@@ -18,26 +18,83 @@ class Header extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-6">
-                        <div className="d-flex justify-content-between">
+                    <div className="col-lg-6 ">
+                        <div className="d-flex justify-content-between ">
                             <div className="header-info-right">
                                 <p>Weekly Attendance</p>
                                 <h3>66%</h3>
                             </div> 
+                            <span class="border-right gray-border"></span>
                             <div className="header-info-right">
                                 <p>Total Views</p>
                                 <h3>45.6K</h3>
                             </div> 
+                            <span class="border-right gray-border"></span>
                             <div className="header-info-right">
                                 <p>Total Revenue</p>
                                 <h3>$44,000</h3>
                             </div>
+                             <span class="border-right gray-border"></span>
                             <div className="message-notification">
                                 <img src="/images/message.png" />
+                                <span className="message-count">2</span>
+
                             </div>
+
+
                         </div>
                     </div>
                 </div>
+
+                <h4 className="text-white pb-3">CREATE SESSION</h4>
+
+                <div className="gray-box">
+                    <div className="session">
+                        <h3 className="info">Session Info</h3>
+                    </div>
+
+                    <div className="container">
+                        <div className="row">
+                                <div className="col-lg-6">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                                <div className="col-lg-3">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                                <div className="col-lg-3">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                        </div>
+
+                        <div className="row mt-4">
+                                <div className="col-lg-6">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                                <div className="col-lg-3">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                                <div className="col-lg-3">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                        </div>
+
+                        <div className="row mt-4">
+                                <div className="col-lg-6">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                                <div className="col-lg-3">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                                <div className="col-lg-3">
+                                    <input type="email" className="form-control"   placeholder="Enter email"/>
+                                </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                
+
             </div>
         )
     }
