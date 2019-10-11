6de0b9aa06ee6ba16c7188fa922b9b6896cc444f
bhudevqversity <bhudev.kashyap@qversity.com>
2019-10-11 13:16:20 +0530

    ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 class Header extends Component {
     render(){
         return(
+
             <div className="container-fluid">
                 <div className="row top-header p-4">
                     <div className="col-lg-2 d-flex d-md-block justify-content-center p-2">
@@ -10,7 +11,7 @@ class Header extends Component {
                     </div>
                     <div className="col-lg-4 d-flex d-md-block justify-content-center p-4">
                         <div className="user-info d-flex align-items-center">
-                            <img className="user-avtar" src="images/attendee.png" />
+                            <img src="images/attendee.png" className="user-avtar"  />
                             <div className="pl-4">
                                 <h3>Welcome Cersei!</h3>
                                 <p>You have 3 sessions this week</p>
@@ -24,17 +25,17 @@ class Header extends Component {
                                 <p>Weekly Attendance</p>
                                 <h3>66%</h3>
                             </div> 
-                            <span class="border-right gray-border"></span>
+                            <span className="border-right gray-border"></span>
                             <div className="header-info-right">
                                 <p>Total Views</p>
                                 <h3>45.6K</h3>
                             </div> 
-                            <span class="border-right gray-border"></span>
+                            <span className="border-right gray-border"></span>
                             <div className="header-info-right">
                                 <p>Total Revenue</p>
                                 <h3>$44,000</h3>
                             </div>
-                             <span class="border-right gray-border"></span>
+                             <span className="border-right gray-border"></span>
                             <div className="message-notification">
                                 <img src="/images/message.png" />
                                 <span className="message-count">2</span>
@@ -53,43 +54,76 @@ class Header extends Component {
                         <h3 className="info">Session Info</h3>
                     </div>
 
-                    <div className="container">
-                        <div className="row">
-                                <div className="col-lg-6">
-                                    <input type="email" className="form-control"   placeholder="Enter email"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <input type="email" className="form-control"   placeholder="Enter email"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <input type="email" className="form-control"   placeholder="Enter email"/>
-                                </div>
+                 <div className="container-fluid register-form">
+            <div className="form">
+                
+                <div className="form-content">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <input type="text" className="input-field" placeholder="Session Name" value=""/>
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="input-field" placeholder="Phone Number *" value=""/>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <input type="text" className="input-field" placeholder="When" value=""/>
+                            </div>
+                            <div className="form-group">
+                                <select className="input-field" id="exampleFormControlSelect1">
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                  <option>5</option>
+                                </select>
+                              </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group input-txt">
+                                Show Participants Signed Up Count on Searches?
+                            </div>
+                            <div className="form-group input-txt">
+                                Show Participants Signed Up Count on Searches?
+                            </div>
                         </div>
 
-                        <div className="row mt-4">
-                                <div className="col-lg-6">
-                                    <input type="email" className="form-control"   placeholder="Enter password"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <input type="email" className="form-control"   placeholder="Enter password"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <input type="email" className="form-control"   placeholder="Enter password"/>
-                                </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <input type="text" className="input-field" placeholder="Session Name" value=""/>
+                            </div>
+                            <div className="form-group">
+                                <select className="input-field" >
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                  <option>5</option>
+                                </select>
+                              </div>
                         </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <input type="text" className="input-field " placeholder="Minimum Participants" value=""/>
+                               
 
-                        <div className="row mt-4">
-                                <div className="col-lg-6">
-                                    <input type="email" className="form-control"   placeholder="Enter email"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <input type="email" className="form-control"   placeholder="Enter email"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <input type="email" className="form-control"   placeholder="Enter email"/>
-                                </div>
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="input-field" placeholder="Confirm Password" value=""/>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <input type="text" className="input-field" placeholder="When" value=""/>
+                            </div>
+                            
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
 
                 </div>
 
