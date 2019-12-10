b27431aa6897902a2413e7d907f1f70a2c497669
lalit <lalit.tiwari@qversity.com>
2019-12-10 17:42:11 +0530

    changes done

--- src/components/home/DemoSessionWine.js
+++ src/components/home/DemoSessionWine.js
@@ -93,11 +93,9 @@ class DemoSessionWine extends Component {
         showParticipant:false,
         amountCharge: '',
         orderWine:false,
-<<<<<<< HEAD
+
         onDemand:false,
-=======
-        orderWineDemand:false,
->>>>>>> abhi-virdioadmin
+        
         hostSessionStart:'',
         participantSessionStart:'',
         minimumNotMet: '',
@@ -2450,11 +2448,7 @@ submitForm = (event) => {
                     </div>
                     <div className="form-group input-txt">
                         <label className="switch">
-<<<<<<< HEAD
                         <input type="checkbox" id = "onDemand" defaultChecked = {this.state.onDemand} onChange = {(e)=>this.setState({[e.target.id]:!this.state.onDemand},()=>console.log("onDemand",this.state.onDemand))} />
-=======
-                        <input type="checkbox" id = "orderWineDemand" defaultChecked = {this.state.orderWineDemand} onChange = {(e)=>this.setState({[e.target.id]:!this.state.orderWineDemand},()=>console.log("orderWineDemand",this.state.orderWineDemand))} />
->>>>>>> abhi-virdioadmin
                         <span className="slider round"></span>
                         </label>
                         <span>Allow Participants on Demand</span>
